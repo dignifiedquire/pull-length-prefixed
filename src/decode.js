a2c448251f9c0a6703964ccb5dd165a9816cba53
areth <al.reshetnikov@gmail.com>
2018-12-11 09:13:18 +0300

    fix: properly handle sync streams

--- src/decode.js
+++ src/decode.js
@@ -20,16 +20,37 @@ function decode (opts) {
 
   return (read) => {
     reader(read)
-    function next () {
-      _decodeFromReader(reader, opts, (err, msg) => {
-        if (err) return p.end(err)
 
-        p.push(msg)
-        next()
-      })
+    // this function has to be written without recursion
+    // or it blow the stack in case of sync stream
+    function next () {
+      let doNext = true
+      let decoded = false
+
+      const decodeCb = (err, msg) => {
+        decoded = true
+        if (err) {
+          p.end(err)
+          doNext = false
+        } else {
+          p.push(msg)
+          if (!doNext) {
+            next()
+          }
+        }
+      }
+
+      while (doNext) {
+        decoded = false
+        _decodeFromReader(reader, opts, decodeCb)
+        if (!decoded) {
+          doNext = false
+        }
+      }
     }
 
     next()
+
     return p
   }
 }
